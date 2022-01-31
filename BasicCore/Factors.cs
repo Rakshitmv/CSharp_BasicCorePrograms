8885cc65e70f36e693e3b019533b9c4b5b29b9d2
Rakshitmv <rakshit8976646711@gmail.com>
2022-01-31 12:08:29 +0530

    [Rakshit] Add: Factots program completed

---
+++ BasicCore/Factors.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    public class Factors
+    {
+        public void Factor()
+        {
+            Console.WriteLine("Enter a number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int i, isPrime;
+
+            for (i = 2; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = 1;
+                    for (int j = 2; j <= i / 2; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = 0;
+                            return;
+                        }
+                    }
+
+                    if (isPrime == 1)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
