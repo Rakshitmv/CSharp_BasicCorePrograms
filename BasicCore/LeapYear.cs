21aa3509ac17f645b0e790582aafa50f061a8326
Rakshitmv <rakshit8976646711@gmail.com>
2022-01-31 11:27:09 +0530

    [Rakshit] Add: Leap year program completed

---
+++ BasicCore/LeapYear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    public class LeapYear
+    {
+        public void Leap()
+        {
+            Console.WriteLine("Enter the year\n");
+            string input = Console.ReadLine();
+            if (input.Length != 4)
+               {
+                  Console.WriteLine("Enter The COrrect Year\n");
+                }
+            else
+               {
+                  int year = Convert.ToInt32(input);
+                  if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                     {
+                        Console.WriteLine("Yes!! It is a Leap Year\n");
+                     }
+                  else
+                     {
+                        Console.WriteLine("No!! It is  not a Leap Year\n");
+                     }
+               }
+        }
+    }
+}
--- BasicCore/Program.cs
+++ BasicCore/Program.cs
@@ -13,7 +13,7 @@ namespace BasicCore
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter 1 to select Replace String\n" + "Enter 2 to Flip a Coin\n" + "Enter 3 to exit\n");
+                Console.WriteLine("Enter 1 to select Replace String\n" + "Enter 2 to Flip a Coin\n" + "Enter 3 to Check For leap Year\n" + "Enter 4 to exit\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -26,6 +26,10 @@ namespace BasicCore
                         flipCoin.Toss();
                         break;
                     case 3:
+                        LeapYear leapYear = new LeapYear();
+                        leapYear.Leap();
+                        break;
+                    case 4:
                         flag = false;
                         break;
 
