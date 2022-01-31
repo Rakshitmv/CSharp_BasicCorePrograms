8fab60a9838b0ae3fb319c5bd8a08bc87086e75d
Rakshitmv <rakshit8976646711@gmail.com>
2022-01-31 10:44:42 +0530

    [Rakshit] Add: Replace String program solved

---
+++ BasicCore/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter 1 to select Replace String\n" + "Enter 2 to exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        ReplaceString replaceString = new ReplaceString();
+                        replaceString.ReplaceStr();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ BasicCore/ReplaceString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    public class ReplaceString
+    {
+        public void ReplaceStr()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            string text = "Hello <<UserName>>, How are you?";
+            string replacedText = text.Replace("<<UserName>>", name);
+            Console.WriteLine(replacedText);
+        }
+    }
+}
